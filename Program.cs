35e4356d131339f95771d995e571b2b91f880583
Jerrified <oyedelejeremiah.ng@gmail.com>
2024-03-12 23:46:55 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+var names = new List<string>
+{
+    "<name>",
+    "Ana",
+    "Felipe",
+    "Jerry"
+};
+foreach (var name in names)
+{
+  Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+Console.WriteLine($"My name is {names[0]}.");
+Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
